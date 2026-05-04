23adfddf7a12806e4fe9d7e125f7b22f5402a134
Nicolas Kole <dal029608@utdallas.edu>
2026-05-04 04:37:58 -0500

    Reset timer each new game feature

--- src/connect4game.py
+++ src/connect4game.py
@@ -146,6 +146,7 @@ class Game:
         self.selected_col = 0
         self.game_over = False
         self.winner = None
+        self.turn_start_time = time.time()
 
     def switch_turn(self):
         self.turn = 2 if self.turn == 1 else 1
